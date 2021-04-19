28f6d11054121666cbaba3219d1f1c2f55e1f6e2
imperative-scientist-4600 <nelom3@gmail.com>
2021-04-19 17:22:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :index
+  end
+  
+  get '/new' do 
+    erb :display_puppy
+  end
+  
+  post '/puppy' do
+    erb 
 
 end
